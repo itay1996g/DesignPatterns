4421dcc5bb5462437ae48c8c92ec57e7ab73b1bb
itay1996g <itay1996g@gmail.com>
2019-07-14 14:16:38 +0300

    Add files via upload

---
+++ FactoryMethod/Seguie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Seguie : Vehicle
+    {
+        protected string drivingWheelHeater;
+
+        public Seguie(string model, float cylinder, string color, int numberOfWheels, string drivingWheelHeater) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.drivingWheelHeater = drivingWheelHeater;
+        }
+
+        public override string ToString()
+        {
+            return "Seguie " + base.ToString() + $" {drivingWheelHeater}";
+        }
+    }
+}
---
+++ FactoryMethod/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Track : Vehicle
+    {
+        protected string drivingWheelHeater;
+
+        public Track(string model, float cylinder, string color, int numberOfWheels, string drivingWheelHeater) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.drivingWheelHeater = drivingWheelHeater;
+        }
+
+        public override string ToString()
+        {
+            return "Track " + base.ToString() + $" {drivingWheelHeater}";
+        }
+    }
+}
