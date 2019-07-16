ffb50dad1906b6996f00bf192b6af08a739588fa
itay1996g <itay1996g@gmail.com>
2019-07-16 10:28:11 +0300

    Add files via upload

---
+++ Visitor/Visitor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShopRent shop = new ShopRent();
+
+            Console.WriteLine(shop);
+
+            IVisitor shekel = new OneShekelSale();
+
+            Console.WriteLine($"Price after tax = {shop.Accept(shekel)}");
+
+            
+        }
+    }
+}
---
+++ Visitor/Visitor/ShopRent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public class ShopRent : IVisitable
+    {
+
+        public double price = 1000;
+
+        public double Accept(IVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
