9538afabfbffdfdc3d6ed53e21691e12dfae11dd
itay1996g <itay1996g@gmail.com>
2019-07-16 14:53:16 +0300

    Add files via upload

---
+++ Memento/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void Load(RichTextBox richText)
+        {
+            richText.Text = careTake.GetLast().savedText;
+        }
+
+
+        public void Previous(RichTextBox richText)
+        {
+            richText.Text = careTake.Redo().savedText;
+        }
+    }
+}
---
+++ Memento/TextCareTaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> mementos = new List<TextMemento>();
+        private int index = 0;
+
+        public void AddMemento(TextMemento memento)
+        {
+            if (index - mementos.Count < 0)
+                this.mementos.RemoveRange(index + 1, this.mementos.Count - index - 1);
+            else
+                this.mementos.RemoveRange(index, this.mementos.Count - index);
+
+            this.mementos.Add(memento);
+            this.index++;
+            
+        }
+
+        public TextMemento GetLast()
+        {
+            if (index >= 1)
+                this.index--;
+            return this.mementos[index];
+        }
+
+        public TextMemento Redo()
+        {
+            if(index <= mementos.Count - 2)
+                this.index++;
+            
+            return this.mementos[index];
+        }
+    }
+}
