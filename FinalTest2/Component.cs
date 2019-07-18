1c359a0de778137842ff8682939cce14cd123ab4
itay1996g <itay1996g@gmail.com>
2019-07-18 16:40:02 +0300

    Add files via upload

---
+++ FinalTest2/Component.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest2
+{
+    public abstract class Component
+    {
+        public int num;
+
+        protected Component(int num)
+        {
+            this.num = num;
+        }
+
+        public abstract void AddChild(Component c);
+
+        public abstract void RemoveChild(Component c);
+
+        public abstract IList<Component> GetChilds();
+
+        public abstract void Draw(string space);
+
+        public abstract int Sum();
+
+    }
+}
---
+++ FinalTest2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest2
+{
+    class Program
+    {
+        static bool isEven(Component c)
+        {
+            if (c.GetChilds() == null && c.num % 2 == 0)
+                return true;
+            else if (c.num % 2 != 0)
+                return false;
+            return isEven(c.GetChilds()[0]) && isEven(c.GetChilds()[1]);
+        }
+
+        static void Main(string[] args)
+        {
+            Component root = new Composite(4);
+            Component circle1 = new Leaf(2);
+            Component rectangle = new Leaf(2);
+
+            root.AddChild(circle1);
+            root.AddChild(rectangle);
+
+            // Checking sum function
+            int sum = root.Sum();
+            Console.WriteLine(sum);
+
+            // Checking isEven function
+            bool a = isEven(root);
+            Console.WriteLine(a);
+
+        }
+    }
+}
